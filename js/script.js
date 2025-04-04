9c57abc0f06cf5b4cb774ad1b2f8d282a9f0858e
Satyam Kumar <111216715+satyam1436@users.noreply.github.com>
2025-04-04 14:04:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -73,7 +73,8 @@ let playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`/songs/`)
+    let a = await fetch(`https://github.com/satyam1436/Spotify-Clone/tree/main/songs/`)
+    console.log("displayAlbums fetch", a);
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
